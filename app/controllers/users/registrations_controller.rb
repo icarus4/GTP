53ed3cb51eeb3a8817f72a32fee33956ee8194d9
icarus4 <icarus4.chu@gmail.com>
2016-05-23 20:37:31 +0800

    1. 註冊時一併建立公司資料 2. 登入密碼應顯示'*'

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,18 +5,13 @@ class Users::RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, notice: 'Account created'
-    else
-      render :new
-    end
+    @company = Company.new(name: params[:company_name].strip, status: 'active')
+    render :new and return if !@company.save
+
+    @user = User.new(email: params[:email].strip, password: params[:password], password_confirmation: params[:password_confirmation], company: @company)
+    render :new and return if !@user.save
+
+    session[:user_id] = @user.id
+    redirect_to root_path, notice: 'Account created'
   end
-
-  private
-
-    def users_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
 end
