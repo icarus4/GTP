3274b5e5eca6e42d0c99838dce3c264b454a5519
icarus4 <icarus4.chu@gmail.com>
2016-09-04 21:55:47 +0800

    Order payment methods by id

--- app/controllers/api/v1/payment_methods_controller.rb
+++ app/controllers/api/v1/payment_methods_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PaymentMethodsController < Api::V1::BaseController
   def index
-    payment_methods = current_company.payment_methods.select(:id, :name)
+    payment_methods = current_company.payment_methods.select(:id, :name).order(:id)
     @payment_methods = payment_methods.map do |pm|
       {
         id:      pm.id,
