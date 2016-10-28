e67592b5edf34c6757bf2e4e80cec8af24e0ad73
icarus4 <icarus4.chu@gmail.com>
2016-10-28 12:42:28 +0800

    [BF] wrong syntax causes exception

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -156,9 +156,9 @@ class PurchaseOrder < Order
   # total / (1 / ((100 + tax_rate) / 100 - 1) + 1) = tax
   def calculate_total_tax
     self.total_tax = if tax_exclusive?
-                       line_items.reduce { |total_tax, line_item| line_item.total * line_item.tax_rate / 100 }.round(2)
+                       line_items.reduce(0) { |total_tax, line_item| line_item.total * line_item.tax_rate / 100 }.round(2)
                      else
-                       line_items.reduce { |total_tax, line_item| line_item.total / (1 / ((100 + line_item.tax_rate) / 100 - 1) + 1) }.round(2)
+                       line_items.reduce(0) { |total_tax, line_item| line_item.total / (1 / ((100 + line_item.tax_rate) / 100 - 1) + 1) }.round(2)
                      end
   end
 
