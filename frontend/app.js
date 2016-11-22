0d1858dca178a8b2c157134d3ff400e5193cc55c
icarus4 <icarus4.chu@gmail.com>
2016-11-22 23:46:13 +0800

    Integrate browserify

---
+++ frontend/app.js
@@ -0,0 +1,17 @@
+// VENDOR
+import Vue from 'vue'
+import Axios from 'axios'
+
+// COMPONENTS
+import TodoList from './components/todo-list.vue'
+
+// Add Rails' CSRF token header to requests
+Axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+
+// GLOBAL COMPONENT
+new Vue({
+  el: '#app',
+  components: {
+    'todo-list': TodoList
+  }
+})
