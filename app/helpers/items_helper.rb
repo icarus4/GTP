5f2fb47294fee54e83b2ccb7b8953c6b852f6380
icarus4 <icarus4.chu@gmail.com>
2017-02-04 23:47:40 +0800

    Remove annotate info from helpers

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,26 +1,2 @@
-# == Schema Information
-#
-# Table name: items
-#
-#  id                   :integer          not null, primary key
-#  company_id           :integer          not null
-#  item_series_id       :integer
-#  available_count      :integer          default(0), not null
-#  on_hand_count        :integer          default(0), not null
-#  cost_per_unit        :integer
-#  wholesale_price      :integer
-#  retail_price         :integer
-#  status               :integer          default(0), not null
-#  weight_unit          :integer
-#  weight_value         :decimal(10, 2)
-#  manufactured_by_self :boolean          default(FALSE), not null
-#  expirable            :boolean          default(TRUE), not null
-#  sku                  :text
-#  name                 :text             default(""), not null
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  image                :string
-#
-
 module ItemsHelper
 end
--- app/helpers/variants_helper.rb
+++ app/helpers/variants_helper.rb
@@ -1,15 +1,2 @@
-# == Schema Information
-#
-# Table name: variants
-#
-#  id                 :integer          not null, primary key
-#  item_id            :integer          not null
-#  quantity           :integer          default(0), not null
-#  manufacturing_date :date
-#  expiry_date        :date
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#
-
 module VariantsHelper
 end
